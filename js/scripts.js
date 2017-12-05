fc265a7ef9b30dc9885fef5886aa9f23c8f2ebb4
Chris Huson and James Donlan <chris@>
2017-12-05 13:25:20 -0800

    word puzzle complete, now takes input and replaces letters before returning

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,18 @@ $(document).ready(function() {
   $("form#puzzle").submit(function(event) {
     event.preventDefault();
     var wordsToScramble = $("input#userSentence").val()
-console.log(wordsToScramble);
+    var splitSentence = wordsToScramble.split("")
+console.log(splitSentence);
+for (var i = 0; i < splitSentence.length; i += 1) {
 
+  if ((splitSentence[i] === "a") || (splitSentence[i] === "e") || (splitSentence[i] === "i") || (splitSentence[i] === "o") || (splitSentence[i] === "u")) {
+    splitSentence[i] = "-";
+  }
+}
+
+var newSentence = splitSentence.join("");
+
+$("#output").text(newSentence);
+$("#results").show();
   });
 });
